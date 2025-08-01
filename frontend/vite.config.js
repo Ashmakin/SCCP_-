f36dac710346a787889c002214f276596114dc8e
Ashmakin <168038272+Ashmakin@users.noreply.github.com>
2025-08-02 05:05:26 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl' // 导入插件
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     basicSsl() // 使用插件
-  ],
-  server: {
-    host: '0.0.0.0', // 允许通过局域网IP访问
-    https: false,     // 启用HTTPS
-  }
 })
