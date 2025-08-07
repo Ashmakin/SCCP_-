571a7548c9f014301d375af9f91d0c699f4c7251
Ashmakin <168038272+Ashmakin@users.noreply.github.com>
2025-08-08 03:50:49 +0800

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ]
+  plugins: [react()],
+  define: {
+    // This replaces any instance of 'global' with 'window' in the build
+    'global': 'window',
+  },
 })
